f97354cf06dcbe60794347b4a99367cfa4b353f9
Shree <Shree@DESKTOP-C63B4ID>
2020-09-17 16:56:12 +0530

    change in class

--- Pro-MyGit/src/MyHome/MainClass.java
+++ Pro-MyGit/src/MyHome/MainClass.java
@@ -2,6 +2,9 @@ package MyHome;
 
 public class MainClass {
 	
+	public void m1() {
+		System.out.println("this is m1()");
+	}
 	public static void main(String[] args) {
 		System.out.println("WelCome In Class");
 	}
