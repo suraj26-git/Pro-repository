cd979eb93cea86bf7bcd36d793fdec220567e340
suraj26-git <71366979+suraj26-git@users.noreply.github.com>
2020-09-17 17:03:53 +0530

    Update MainClass.java
    by git account

--- Pro-MyGit/src/MyHome/MainClass.java
+++ Pro-MyGit/src/MyHome/MainClass.java
@@ -5,6 +5,10 @@ public class MainClass {
 	public void m1() {
 		System.out.println("this is m1()");
 	}
+	
+	public void m2() {
+		System.out.println("this is m2()");
+	}
 	public static void main(String[] args) {
 		System.out.println("WelCome In Class");
 	}
